6637a0a6bb10f94ce82ec7ea16d84eb2f5da288a
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-04-30 11:49:35 -0600

    Working on it

--- js/date.js
+++ js/date.js
@@ -1,6 +1,8 @@
 
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+let options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('current_date').textContent = new Date().toLocaleDateString('en-US',options);        
 
-let copyright = "\u00A9 2022 -Katie Blunck- Georgia";
+copyright_year = new Date().getFullYear()
+let copyright = `\u00A9 ${copyright_year} -Katie Blunck- Georgia`;
+
 document.getElementById('copyright').textContent = copyright;
