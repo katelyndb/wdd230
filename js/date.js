0a33653d1fd857b28c43dc13c43ae4b7c1e15cdb
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-05-06 14:45:06 -0600

    Delete date.js

--- js/date.js
+++
@@ -1,8 +0,0 @@
-
-let options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('current_date').textContent = new Date().toLocaleDateString('en-US',options);        
-
-copyright_year = new Date().getFullYear()
-let copyright = `\u00A9 ${copyright_year} -Katie Blunck- Georgia`;
-
-document.getElementById('copyright').textContent = copyright;
