4887d254562481254b1d1ef84d1916d9c4bd8a97
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-05-19 22:07:17 -0600

    Stuff

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -8,6 +8,6 @@ let options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('current_date').textContent = new Date().toLocaleDateString('en-US',options);        
 
 copyright_year = new Date().getFullYear()
-let copyright = `\u00A9 ${copyright_year} -Katie Blunck- Georgia`;
+let copyright = `\u00A9 ${copyright_year} || Katie Blunck |`;
 
 document.getElementById('copyright').textContent = copyright;
