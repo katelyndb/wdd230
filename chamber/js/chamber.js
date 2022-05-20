904a635262bcf60bea78208abd08913f4d8e58cf
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-05-20 17:25:58 -0600

    FInsihed this weeks!

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -2,8 +2,6 @@ function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
-
-
 let options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('current_date').textContent = new Date().toLocaleDateString('en-US',options);
 document.getElementById('header_date').textContent = new Date().toLocaleDateString('en-US',options);      
@@ -12,3 +10,12 @@ copyright_year = new Date().getFullYear()
 let copyright = `\u00A9 ${copyright_year} | Katie Blunck | `;
 
 document.getElementById('copyright').textContent = copyright;
+
+const dayOfWeek = new Date().getDay();
+var notifBanner = document.getElementById('notif_banner');
+if (dayOfWeek == 1 || dayOfWeek == 2 {
+    notifBanner.style.display = 'block';
+}
+
+
+
