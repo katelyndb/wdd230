d7fd0a03065995310f4697e694d887d21277fa4c
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-05-19 22:37:32 -0600

    BOM

---
+++ BOM/bom-top-25/README.md
@@ -0,0 +1,4 @@
+# BOM Top 25
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/katelyndb/pen/vYdZNwd](https://codepen.io/katelyndb/pen/vYdZNwd).
+
---
+++ BOM/bom-top-25/dist/script.js
@@ -0,0 +1,25 @@
+// Creates the variables
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+//Function on click of button
+button.addEventListener("click", () => {
+	const myItem = input.value;
+	input.value = "";
+	//Creates each element that makes up box
+	const listItem = document.createElement("li");
+	const listText = document.createElement("span");
+	const listButton = document.createElement("button");
+	//Adds the information to the box
+	listItem.appendChild(listText);
+	listText.textContent = myItem;
+	listItem.appendChild(listButton);
+	listButton.textContent = "❌";
+	list.appendChild(listItem);
+	//If the X button is pressed, removes the entire thing
+	listButton.addEventListener("click", () => {
+		list.removeChild(listItem);
+	});
+	//Keeps cursor running in box
+	input.focus();
+});
