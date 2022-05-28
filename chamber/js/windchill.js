3edb5a206bc8deda801a9b951b65eb1163c44491
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-05-28 09:10:19 -0600

    aghhhhhhhhhh

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,10 +1,21 @@
 
+temp = 60;
+windSpeed = 15;
+
+if ((temp <= 50)&& (windSpeed > 3 )){ 
+    calculateWindChill(temp, windSpeed);
+}
+else {
+    windChillFactor = "N/A";
+}
+
+
 function calculateWindChill(temp, windSpeed){
-temp = parseInt(temp)
-windSpeed = parseInt(windSpeed)
-if ((temp <= 50)&& (windSpeed > 3 )){
-    windChillFactor = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, .16))+ (.4275 * temp * Math.pow(windSpeed, .16))
-}else {
-    windChillFactor = "NA"
+temp = parseInt(temp);
+windSpeed = parseInt(windSpeed);
+windChillFactor = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, .16))+ (.4275 * temp * Math.pow(windSpeed, .16));
 }
-}
+
+document.getElementById('windChill').textContent = windChillFactor;
+document.getElementById('temp').textContent =`${temp} ${<span>&#176;</span>} F`;
+document.getElementById('windSpeed').textContent = `${windSpeed} mph`;
