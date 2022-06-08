64ea18e935559735dada2476cc3c16737f533f90
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-06-07 23:25:42 -0600

    wefbashjsdgvbhjfhbjshbjfsdhbjsdfhbjsdfhbjsdfhbjdxchbjzsdxchbzdxchbvadshbj

--- chamber/discover/js/discover.js
+++ chamber/discover/js/discover.js
@@ -1,7 +1,24 @@
 let n = localStorage.getItem('n');
+let prevdate = localStorage.getItem('prevdate');
 
 if (n == null){
     n = 0;
 }
 localStorage.setItem('n', ++n);
 document.getElementById('user_visits').innerHTML = localStorage.getItem('n');
+
+
+var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+difference =    prevdate;
+
+
+localStorage.setItem('prevdate', currentdate);
+
+document.getElementById('user_visited').innerHTML = difference;
