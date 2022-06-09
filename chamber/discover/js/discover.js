cbc1b88d96836b5d5554f1c7788b27d8268982c3
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-06-08 20:57:52 -0600

    fixing more code

--- chamber/discover/js/discover.js
+++ chamber/discover/js/discover.js
@@ -10,6 +10,8 @@ document.getElementById('user_visits').innerHTML = localStorage.getItem('n');
 
 
 var currentdate = new Date(); 
+var currentday = currentdate.getDate();
+var currentmonth = currentdate.getMonth()+1;
 var datetime = "Last Sync: " + currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/" 
                 + currentdate.getFullYear() + " @ "  
@@ -18,10 +20,29 @@ var datetime = "Last Sync: " + currentdate.getDate() + "/"
                 + currentdate.getSeconds();
 
 difference = prevmonth;
+if (prevmonth == currentmonth){
+    difference = currentday - prevday;
+}
+else if (currentmonth - prevmonth == 1 ){
+    if (prevmonth == 2){
+        difference = 28 - prevday + currentday;
+    }
+    else if (prevmonth == 4  || prevmonth == 6 || prevmonth == 9 || prevmonth == 11){
+        difference = 30 - prevday + currentday;
+    }
+    else {
+        difference = 31 - prevday + currentday;
+    }
+}
+else {
+    difference = 0;
+}
 
 
 
-localStorage.setItem('prevday', currentdate.getDate());
-localStorage.setItem('prevmonth', currentdate.getMonth()+1);
+
+
+localStorage.setItem('prevday', currentday);
+localStorage.setItem('prevmonth', currentmonth);
 
 document.getElementById('user_visited').innerHTML = difference;
