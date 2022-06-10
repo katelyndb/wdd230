77f963c1a2c7ecb9538422d752223144ed0c5012
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-06-10 15:36:59 -0600

    Done with lazy loading!

--- LazyLoad/js/lazyload.js
+++ LazyLoad/js/lazyload.js
@@ -1,18 +1,30 @@
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 const imgOptions = {
-    threshold: 0,
+    threshold: 1,
     rootMargin: "0px 0px 50px 0px"
 };
 
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {image.removeAttribute('data-src');};
-}
+};
 
-of ('IntersectionObserver' in window) {
+if ('IntersectionObserver' in window) {
     const imgObserver = new IntersectionObserver((items,observer) => {
         items.forEach((item) => {
-            loadImages(img);
-        });
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target)
+            }
+
+        })
+    }, imgOptions);
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+}
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
     });
 }
