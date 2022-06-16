dc4da072fc0d7ba8957cccff73db8ac54779529f
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-06-16 17:55:23 -0600

    working on prophet things

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -1,7 +1,50 @@
 
 
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const URL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
+
+async function getProphets() {
+  let response = await fetch(URL)
+  if (response.ok) {
+    let data = await response.json();
+    //console.log(data);
+    makeProphetCards(data);
+
+  }else{
+    throw Error(response.statusText);
+  }
+}
+
+async function makeProphetCards(data) {
+  console.log(data);
+  data.prophets.forEach(prophet => {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let portrait = document.createElement('img');
+    let date = document.createElement('p');
+    let place = document.createElement('p');
+
+    h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President`);
+    portrait.setAttribute('loading', 'lazy');
+    date.innerHTML =`Date of Birth: ${prophet.birthdate}`;
+    
+    card.append(h2);
+    card.append(place);
+    card.append(date);
+    card.append(portrait);
+    cards.append(card);
+  });
+}
+
+
+getProphets();
+
+
+
+
+/*
 
 fetch(requestURL)
   .then(function (response) {
@@ -33,8 +76,8 @@ fetch(requestURL)
     place.textContent = `Place of Birth: ${prophet.birthplace}`
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     ____.setAttribute('src', prophet.imageurl);
-    ____.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President`);
-    ____.setAttribute('loading', 'lazy');
+    ____.setAttribute();
+    ____
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
@@ -45,3 +88,4 @@ fetch(requestURL)
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
   }
+  */
