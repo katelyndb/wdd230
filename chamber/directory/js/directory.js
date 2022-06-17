7666b908de5a1afe86d38e8ecca19323259f80c0
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-06-16 20:40:28 -0600

    Working on the directory page

---
+++ chamber/directory/js/directory.js
@@ -0,0 +1,32 @@
+const URL = '../data.json'; 
+const cards = document.querySelector('.cards');
+
+makeCompanyCards(URL);
+
+
+async function makeCompanyCards(data) {
+    console.log(data);
+    data.companies.forEach(company => {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let icon = document.createElement('img');
+    let p = document.createElement('p');
+    let pmember = document.createElement('p');
+    let a = document.createElement('a');
+
+    h2.innerHTML = `${company.name}`;
+    icon.setAttribute('src', company.iconname);
+    icon.setAttribute('alt', `Icon of  ${company.name}`);
+    icon.setAttribute('loading', 'lazy');
+    p.innerHTML =`Address: ${company.adress} <br> Phone Number: ${company.phonenum}`;
+    a.setAttribute('href', company.weburl);
+    a.setAttribute('value', "DID THIS WORK?");
+    pmember.innerHTML = `Membership Level: ${company.membership} <br> Years as a valued member: <strong>${company.memberyear} </strong>`;
+    card.append(h2);
+    card.append(icon);
+    card.append(a);
+    card.append(p);
+    card.append(pmember);
+    cards.append(card);
+  });
+}
