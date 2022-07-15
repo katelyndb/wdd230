5a1cbf9ec2aa0a624b42b98460ddb44ad87b62b8
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-07-14 18:27:28 -0600

    starting this nightmare

---
+++ final/js/base.js
@@ -0,0 +1,6 @@
+
+//Getting Copyright information for Footer
+let copyright_year = new Date().getFullYear();
+document.getElementById('copyright').textContent = `\u00A9 ${copyright_year} Temple Inn and Suites Inc.`;
+//
+
