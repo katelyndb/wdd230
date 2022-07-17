d6937d87e5ab3de403ca335d412053515dedfe97
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-07-17 16:37:36 -0600

    Working on the stufffffffff

--- final/js/base.js
+++ final/js/base.js
@@ -4,3 +4,9 @@ let copyright_year = new Date().getFullYear();
 document.getElementById('copyright').textContent = `\u00A9 ${copyright_year} Temple Inn and Suites Inc.`;
 //
 
+//Opening and Closing the Nav bar menu
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+//
+
